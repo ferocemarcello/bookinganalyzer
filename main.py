1b3b98989bb5259d59b64939d8976202034e052e
Marcello Feroce <marcello.feroce@tum.de>
2019-10-22 16:22:42 +0200

    test python ssh

--- main.py
+++ main.py
@@ -17,4 +17,5 @@ if __name__ == '__main__':
     #lemm.lemm.stemlemmatizer("booking_keywords.txt")
     #csvwriter.do("booking_keywords.txt")
     #wordwriter.wordwriter.write("booking_keywords.txt")
-    topicwriter.TopicWriter().do("booking_keywords.txt")
+    #topicwriter.TopicWriter().do("booking_keywords.txt")
+    print("this is a test")
