090d0c7dd5ece2b3ed5e008b39bf60ff2b642f76
Marcello Feroce <marcello.feroce@tum.de>
2019-11-14 20:15:39 +0100

    updated topic modeling with bow

--- helper.py
+++ helper.py
@@ -25,9 +25,8 @@ def getRawCorpus(csv_file, id_and_country=False, additionaldetails=False):
         db.connect()
         for row in reader:
             i += 1
-            if i % 1000 == 0:
+            if i % 50000 == 0:
                 print('reading sentence ' + str(i))
-                break
             id=row[0]
             query = 'SELECT HotelNumber, FamilyType FROM masterthesis.reviews WHERE ReviewID='+id
             det = queryexecutor.execute(query=query)
