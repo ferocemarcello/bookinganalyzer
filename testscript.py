2122677cd6bf62829889b14df6b8ef3cf0652ca6
Marcello Feroce <marcello.feroce@tum.de>
2019-11-16 21:03:30 +0100

    updated topic modeling with bow using stanford

--- testscript.py
+++ testscript.py
@@ -1,40 +1,7 @@
 import time
 import nltk
 import spacy
-import os,sys
-import subprocess
-from contextlib import contextmanager
-from pycorenlp import StanfordCoreNLP
-@contextmanager
-def suppress_stdout():
-    with open(os.devnull, "w") as devnull:
-        old_stdout = sys.stdout
-        sys.stdout = devnull
-        try:
-            yield
-        finally:
-            sys.stdout = old_stdout
-os.system('kill $(lsof -t -i:9000)')
-os.chdir('./resources/stanford-corenlp-full-2018-10-05')
-cmd = 'java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -annotators "tokenize,ssplit,pos,lemma,parse,sentiment" -port 9000 -timeout 26000 &'
-with open(os.devnull, "w") as f:
-    subprocess.call(cmd, shell=True,stderr=f,stdout=f)
-    os.chdir('../../')
-    nlp_wrapper = StanfordCoreNLP('http://localhost:9000')
-    start_time = time.time()
-    doc = "The chocolates great!"
-    annot_doc = nlp_wrapper.annotate(doc,
-        properties={
-            'annotators': 'lemma, pos',
-            'outputFormat': 'json',
-            'timeout': 100000,
-        })
-f.close()
-annot_doc=annot_doc['sentences'][0]['tokens']
-print(str(time.time() - start_time))
-os.system('kill $(lsof -t -i:9000)')
-print("cacca")
-'''nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 f = open("resources/bow/allfreq/breakfast_good.txt", "r")
 tot=0
 notnounspacy=[]
@@ -61,4 +28,4 @@ f.close()
 print(len(alltok))
 print(len(notnounspacy))
 print(len(notnounnltk))
-print("cacca")'''
+print("cacca")
