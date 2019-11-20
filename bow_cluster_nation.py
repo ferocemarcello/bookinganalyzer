863b2effadddc8530b1c24dc96655fd7dbb73600
Marcello Feroce <marcello.feroce@tum.de>
2019-11-20 09:03:50 +0100

    updated topic modeling with bow using stanford

--- bow_cluster_nation.py
+++ bow_cluster_nation.py
@@ -9,22 +9,26 @@ def cluster(csv_reader):
     maxlentokens = firstrow.count('')
     firstrow = firstrow[maxlentokens:]
     topk = len(firstrow)
-    country_cluster={}
-    i=0
+    country_cluster_hotel={}
+    country_cluster_tourist = {}
+    clusters=[country_cluster_hotel, country_cluster_tourist]
     for row in csv_reader:
-        i+=1
-        country=row[5]
+        country_hot=row[5]
+        country_tour = row[1]
+        countries=[country_hot,country_tour]
         values=row[maxlentokens:]
         values=list(map(int, values))
-        if country not in country_cluster.keys():
-            country_cluster[country]={}
-            country_cluster[country]['sum']=[0]*len(values)
-            country_cluster[country]['count_rev']=0
-        country_cluster[country]['count_rev']+=1
-        country_cluster[country]['sum']=numpy.add(country_cluster[country]['sum'],values)
-    for country in country_cluster.keys():
-        country_cluster[country]['rel_freq']=[x/country_cluster[country]['count_rev'] for x in country_cluster[country]['sum']]
-    return firstrow,country_cluster
+        for i in range(2):
+            if countries[i] not in clusters[i].keys():
+                clusters[i][countries[i]]={}
+                clusters[i][countries[i]]['sum']=[0]*len(values)
+                clusters[i][countries[i]]['count_rev']=0
+            clusters[i][countries[i]]['count_rev']+=1
+            clusters[i][countries[i]]['sum']=numpy.add(clusters[i][countries[i]]['sum'], values)
+    for cc in [country_cluster_hotel, country_cluster_tourist]:
+        for country in cc.keys():
+            cc[country]['rel_freq']=[x/cc[country]['count_rev'] for x in cc[country]['sum']]
+    return firstrow,country_cluster_hotel,country_cluster_tourist
 def do(originfile):
     keywords = helper.getKeywords(originfile)
     for emotion in ['Good', 'Bad']:
@@ -36,19 +40,28 @@ def do(originfile):
             try:
                 with open('resources/bow/'+keyword+'_'+emotion.lower()+'.csv') as csv_file:
                     csv_reader = csv.reader(csv_file, delimiter='|')
-                    tokens,country_cluster=cluster(csv_reader)
+                    tokens,country_cluster_hotel,country_cluster_tourist=cluster(csv_reader)
                 csv_file.close()
             except:
                 goforward=False
             if goforward:
-                if not os.path.exists('resources/bow/country_freq/'):
-                    os.makedirs('resources/bow/country_freq/')
-                with open('resources/bow/country_freq/' + keyword + '_' + emotion.lower() + '.csv', mode='w') as file:
+                if not os.path.exists('resources/bow/country_freq/byhotelcountry/'):
+                    os.makedirs('resources/bow/country_freq/byhotelcountry/')
+                if not os.path.exists('resources/bow/country_freq/bytouristcountry/'):
+                    os.makedirs('resources/bow/country_freq/bytouristcountry/')
+                with open('resources/bow/country_freq/byhotelcountry/' + keyword + '_' + emotion.lower() + '.csv', mode='w') as file:
                     writer = csv.writer(file, delimiter='|', quotechar='"',
                                         quoting=csv.QUOTE_MINIMAL)
-                    writer.writerow([''] * 1 + tokens)
-                    for country in country_cluster.keys():
-                        writer.writerow([country]+country_cluster[country]['rel_freq'])
+                    writer.writerow([''] * 2 + tokens)
+                    for country in country_cluster_hotel.keys():
+                        writer.writerow([country]+[country_cluster_hotel[country]['count_rev']]+list(map("{:.15f}".format, country_cluster_hotel[country]['rel_freq'])))
+                file.close()
+                with open('resources/bow/country_freq/bytouristcountry/' + keyword + '_' + emotion.lower() + '.csv', mode='w') as file:
+                    writer = csv.writer(file, delimiter='|', quotechar='"',
+                                        quoting=csv.QUOTE_MINIMAL)
+                    writer.writerow([''] * 2 + tokens)
+                    for country in country_cluster_tourist.keys():
+                        writer.writerow([country]+[country_cluster_tourist[country]['count_rev']]+list(map("{:.15f}".format, country_cluster_tourist[country]['rel_freq'])))
                 file.close()
             print('------------------------------------------------------')
             print(str(time.time() - start_time) + ' seconds to compute ' + keyword + ' ' + emotion)
