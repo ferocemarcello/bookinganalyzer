71c2e66ce4f9edaa15453c87ef1e25f01f370a34
Marcello Feroce <marcello.feroce@tum.de>
2019-11-20 09:13:43 +0100

    updated topic modeling with bow using stanford

--- bagofwordanalyzer.py
+++ bagofwordanalyzer.py
@@ -71,7 +71,7 @@ def analyze(originfile):
     for emotion in ['Good','Bad']:
         print("begin " + emotion)
         for keyword in list(keywords.keys()):
-            if not(emotion=='Good' and keyword=='cleaning'):
+            if emotion=='Good' and keyword=='cleaning':
                 start_time = time.time()
                 print(keyword+' ---- '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
                 raw_corpus = helper.getRawCorpus(
